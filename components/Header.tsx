32ea18f9ca6e38d4fbb6f418c4b03918efb93253
Abolfazl Panahiazar <abolfazl.panahiazar@rambody.me>
2022-08-31 17:39:46 +0430

    Add home banner and fix header

--- components/Header.tsx
+++ components/Header.tsx
@@ -21,100 +21,102 @@ const Header: FC = () => {
   };
 
   return (
-    <header className="w-full h-11 lg:h-20 bg-F2F5F7 px-5 lg:px-14 flex items-center">
-      <div className="w-full h-full flex lg:hidden justify-between items-center">
-        <img src="mj.png" alt="mj barber logo" className="h-9" />
-        <FaBars size={20} onClick={openHamburgerMenu} />
-        <HamburgerMenu
-          isOpen={isHamburgerMenuOpen}
-          closeMenu={closeHamburgerMenu}
-        />
-      </div>
-      <div className="w-full h-full hidden lg:flex items-center">
-        <div className="h-full flex items-center">
-          <a target="_blank" href="https://instagram.com/hairsalon_mj">
-            <AiFillInstagram size={25} color="#B95F1E" className="mr-1" />
-          </a>
-          <a target="_blank" href="https://wa.me/+905323977073">
-            <IoLogoWhatsapp size={25} color="#B95F1E" />
-          </a>
+    <div className="w-full bg-F2F5F7 shadow-md flex justify-center">
+      <header className="w-full max-w-8xl h-11 lg:h-20 px-5 lg:px-14 flex items-center">
+        <div className="w-full h-full flex lg:hidden justify-between items-center">
+          <img src="mj.png" alt="mj barber logo" className="h-9" />
+          <FaBars size={20} onClick={openHamburgerMenu} />
+          <HamburgerMenu
+            isOpen={isHamburgerMenuOpen}
+            closeMenu={closeHamburgerMenu}
+          />
         </div>
-        <div className="flex-grow h-full flex justify-evenly items-center">
-          <Link href="/">
-            <span
-              className={`${
-                asPath === "/" ? "text-B95F1E" : "text-191C62"
-              } text-sm font-bold cursor-pointer hover:underline`}
+        <div className="w-full h-full hidden lg:flex items-center">
+          <div className="h-full flex items-center">
+            <a target="_blank" href="https://instagram.com/hairsalon_mj">
+              <AiFillInstagram size={25} color="#B95F1E" className="mr-1" />
+            </a>
+            <a target="_blank" href="https://wa.me/+905323977073">
+              <IoLogoWhatsapp size={25} color="#B95F1E" />
+            </a>
+          </div>
+          <div className="flex-grow h-full flex justify-evenly items-center">
+            <Link href="/">
+              <span
+                className={`${
+                  asPath === "/" ? "text-B95F1E" : "text-191C62"
+                } text-sm font-bold cursor-pointer hover:underline`}
+              >
+                HOME
+              </span>
+            </Link>
+            <Link href="/packages">
+              <span
+                className={`${
+                  asPath === "/packages" ? "text-B95F1E" : "text-191C62"
+                } text-sm font-bold cursor-pointer hover:underline`}
+              >
+                PACKAGES
+              </span>
+            </Link>
+            <Link href="/book">
+              <span
+                className={`${
+                  asPath === "/book" ? "text-B95F1E" : "text-191C62"
+                } text-sm font-bold cursor-pointer hover:underline`}
+              >
+                BOOK NOW
+              </span>
+            </Link>
+          </div>
+          <div className="h-full flex justify-center items-center">
+            <img src="mj.png" alt="mj barber logo" className="h-14" />
+          </div>
+          <div className="flex-grow h-full flex justify-evenly items-center">
+            <Link href="/gallary">
+              <span
+                className={`${
+                  asPath === "/gallary" ? "text-B95F1E" : "text-191C62"
+                } text-sm font-bold cursor-pointer hover:underline`}
+              >
+                GALLARY
+              </span>
+            </Link>
+            <Link href="/contact">
+              <span
+                className={`${
+                  asPath === "/contact" ? "text-B95F1E" : "text-191C62"
+                } text-sm font-bold cursor-pointer hover:underline`}
+              >
+                CONTACT
+              </span>
+            </Link>
+            <a
+              target="_blank"
+              href="https://www.google.com/maps/search/?api=1&query=41.02949268093128,28.982238165045"
             >
-              HOME
-            </span>
-          </Link>
-          <Link href="/packages">
-            <span
-              className={`${
-                asPath === "/packages" ? "text-B95F1E" : "text-191C62"
-              } text-sm font-bold cursor-pointer hover:underline`}
+              <span
+                className={
+                  "text-191C62 text-sm font-bold cursor-pointer hover:underline"
+                }
+              >
+                LOCATION
+              </span>
+            </a>
+          </div>
+          <div className="h-full flex items-center">
+            <a
+              target="_blank"
+              href="tel:+905323977073"
+              className="flex items-center"
             >
-              PACKAGES
-            </span>
-          </Link>
-          <Link href="/book">
-            <span
-              className={`${
-                asPath === "/book" ? "text-B95F1E" : "text-191C62"
-              } text-sm font-bold cursor-pointer hover:underline`}
-            >
-              BOOK NOW
-            </span>
-          </Link>
+              <IoMdCall size={25} color="#B95F1E" className="mr-1" />
+              <span className="text-B95F1E text-xs">+90 532 397 70 73</span>
+            </a>
+          </div>
         </div>
-        <div className="h-full flex justify-center items-center">
-          <img src="mj.png" alt="mj barber logo" className="h-14" />
-        </div>
-        <div className="flex-grow h-full flex justify-evenly items-center">
-          <Link href="/gallary">
-            <span
-              className={`${
-                asPath === "/gallary" ? "text-B95F1E" : "text-191C62"
-              } text-sm font-bold cursor-pointer hover:underline`}
-            >
-              GALLARY
-            </span>
-          </Link>
-          <Link href="/contact">
-            <span
-              className={`${
-                asPath === "/contact" ? "text-B95F1E" : "text-191C62"
-              } text-sm font-bold cursor-pointer hover:underline`}
-            >
-              CONTACT
-            </span>
-          </Link>
-          <a
-            target="_blank"
-            href="https://www.google.com/maps/search/?api=1&query=41.02949268093128,28.982238165045"
-          >
-            <span
-              className={
-                "text-191C62 text-sm font-bold cursor-pointer hover:underline"
-              }
-            >
-              LOCATION
-            </span>
-          </a>
-        </div>
-        <div className="h-full flex items-center">
-          <a
-            target="_blank"
-            href="tel:+905323977073"
-            className="flex items-center"
-          >
-            <IoMdCall size={25} color="#B95F1E" className="mr-1" />
-            <span className="text-B95F1E text-xs">+90 532 397 70 73</span>
-          </a>
-        </div>
-      </div>
-    </header>
+      </header>
+    </div>
   );
 };
 
